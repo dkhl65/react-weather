13805f1579297fbaeb289f6a794c06737e225631
Daniel Liang <44882266+dkhl65@users.noreply.github.com>
2023-08-19 22:35:14 -0400

    Search Bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
+import SearchBar from "./SearchBar";
+
 function App() {
   return (
     <div className="ui container">
       <h1 className="ui header">React Weather App</h1>
+      <SearchBar />
     </div>
   );
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+function SearchBar({ onSearch }) {
+  const [location, setLocation] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const detectLocation = () => {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const detectedLocation = `${position.coords.latitude}, ${position.coords.longitude}`;
+        setErrorMessage("");
+        setLocation(detectedLocation);
+        if (typeof onSearch === "function") {
+          onSearch(detectedLocation);
+        }
+      },
+      (err) => {
+        setErrorMessage(err.message);
+      }
+    );
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (typeof onSearch === "function") {
+      onSearch(location);
+    }
+  };
+
+  return (
+    <div className="search-bar ui segment">
+      <form className="ui form" onSubmit={handleSubmit}>
+        <div className="field">
+          <label>
+            Enter a city name, decimal latitude and longitude, postal code or IP
+            address
+          </label>
+          <input
+            type="text"
+            className="ui input"
+            placeholder="Search location"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </div>
+        <button className="ui button primary">Go</button>
+        <button className="ui button" type="button" onClick={detectLocation}>
+          Detect Location
+        </button>
+        {errorMessage && (
+          <div className="ui negative message">
+            <i className="close icon" onClick={() => setErrorMessage("")} />
+            <div className="header">{errorMessage}</div>
+          </div>
+        )}
+      </form>
+    </div>
+  );
+}
+
+export default SearchBar;
